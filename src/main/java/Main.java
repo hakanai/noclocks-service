5b670d261edfde4b6fb9aca6f128ddf4ee9b6349
Trejkaz (pen name) <trejkaz@trypticon.org>
2018-09-20 20:08:08 +1000

    Initial local time support

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,20 +2,18 @@ import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufAllocator;
 import io.netty.buffer.ByteBufOutputStream;
 import ratpack.handling.Context;
-import ratpack.render.Renderable;
 import ratpack.server.BaseDir;
 import ratpack.server.RatpackServer;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.time.Instant;
+import java.time.ZoneId;
 import java.time.ZoneOffset;
-import java.time.ZonedDateTime;
 import java.time.temporal.ChronoField;
-import java.util.Arrays;
+import java.time.zone.ZoneOffsetTransition;
+import java.time.zone.ZoneRules;
 
 public class Main {
     private static final long MAX_UNSIGNED_INT = 0xFFFFFFFFL;
@@ -34,45 +32,72 @@ public class Main {
                     String nowParameter = context.getRequest().getQueryParams().get("now");
                     Instant now = nowParameter == null ? Instant.now() : Instant.parse(nowParameter);
 
-                    // TODO: Code obviously breaks in 2037.
-                    long unixTime = now.getLong(ChronoField.INSTANT_SECONDS);
-                    if (unixTime < 0 || unixTime > MAX_UNSIGNED_INT) {
-                        throw new IllegalStateException("Application is now broken, unixTime = " + unixTime);
-                    }
-
-                    // RGBA -> ARGB
-                    int pixel = (int) ((unixTime << 24) | ((unixTime >> 8) & 0xFFFFFF));
-
                     // TODO: Can we reuse the image?
                     BufferedImage image = new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB);
-                    image.setRGB(0, 0, pixel);
-
-                    ByteBuf buffer = ByteBufAllocator.DEFAULT.buffer();
-                    boolean success = false;
-                    try {
-
-                        // TODO: Could avoid using Image I/O and write directly into a buffer ourselves. Maybe in BMP format?
-                        try (ByteBufOutputStream outputStream = new ByteBufOutputStream(buffer)) {
-                            if (!ImageIO.write(image, "PNG", outputStream)) {
-                                throw new IllegalStateException("No PNG writer in JRE!");
-                            }
-                        }
-
-                        context.getResponse().contentType("image/png");
-                        context.getResponse().noCompress();
-                        // TODO: Could possibly ETag it with the value we used.
-                        context.getResponse().getHeaders().add("Cache-Control", "no-cache");
-                        context.getResponse().send(buffer);
-
-                        success = true;
-                    } finally {
-                        if (!success) {
-                            buffer.release();
-                        }
-                    }
+                    setPixelValue(image, 0, 0, getUnixTime(now));
+                    sendImage(context, image);
                 });
 
+                chain.get("api/1/local", context -> {
+
+                    String nowParameter = context.getRequest().getQueryParams().get("now");
+                    Instant now = nowParameter == null ? Instant.now() : Instant.parse(nowParameter);
+                    String zoneParameter = context.getRequest().getQueryParams().get("tz");
+                    ZoneId zoneId = zoneParameter == null ? ZoneOffset.UTC : ZoneId.of(zoneParameter);
+
+                    BufferedImage image = new BufferedImage(4, 1, BufferedImage.TYPE_INT_ARGB);
+                    setPixelValue(image, 0, 0, getUnixTime(now));
+                    ZoneRules zoneRules = zoneId.getRules();
+                    setPixelValue(image, 1, 0, zoneRules.getOffset(now).getTotalSeconds());
+                    ZoneOffsetTransition transition = zoneRules.nextTransition(now);
+                    if (transition != null) {
+                        setPixelValue(image, 2, 0, getUnixTime(transition.getInstant()));
+                        setPixelValue(image, 3, 0, transition.getOffsetAfter().getTotalSeconds());
+                    }
+                    sendImage(context, image);
+                });
             });
         });
-  }
+    }
+
+    private static long getUnixTime(Instant instant) {
+        long unixTime = instant.getLong(ChronoField.INSTANT_SECONDS);
+        if (unixTime < 0 || unixTime > MAX_UNSIGNED_INT) {
+            throw new IllegalStateException("Application is now broken, unixTime = " + unixTime);
+        }
+        return unixTime;
+    }
+
+    private static void setPixelValue(BufferedImage image, int x, int y, long value) {
+        // RGBA -> ARGB
+        int pixel = (int) ((value << 24) | ((value >> 8) & 0xFFFFFF));
+
+        image.setRGB(x, y, pixel);
+    }
+
+    private static void sendImage(Context context, BufferedImage image) throws IOException {
+        ByteBuf buffer = ByteBufAllocator.DEFAULT.buffer();
+        boolean success = false;
+        try {
+
+            // TODO: Could avoid using Image I/O and write directly into a buffer ourselves. Maybe in BMP format?
+            try (ByteBufOutputStream outputStream = new ByteBufOutputStream(buffer)) {
+                if (!ImageIO.write(image, "PNG", outputStream)) {
+                    throw new IllegalStateException("No PNG writer in JRE!");
+                }
+            }
+
+            context.getResponse().contentType("image/png");
+            context.getResponse().noCompress();
+            // TODO: Could possibly ETag it with the value we used.
+            context.getResponse().getHeaders().add("Cache-Control", "no-cache");
+            context.getResponse().send(buffer);
+
+            success = true;
+        } finally {
+            if (!success) {
+                buffer.release();
+            }
+        }
+    }
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -27,7 +27,7 @@ public class MainTest {
     }
 
     @Test
-    public void test() throws Exception {
+    public void testUtc() throws Exception {
         ReceivedResponse response = appUnderTest.getHttpClient().get("/api/1/utc?now=2007-08-31T00:00:00Z");
         assertThat(response.getHeaders().get("Content-Type"), is(equalTo("image/png")));
         assertThat(response.getHeaders().get("Cache-Control"), is(equalTo("no-cache")));
@@ -36,13 +36,31 @@ public class MainTest {
             BufferedImage image = ImageIO.read(body);
             assertThat(image.getWidth(), is(1));
             assertThat(image.getHeight(), is(1));
-
-            int pixel = image.getRGB(0, 0);
-
-            // ARGB -> RGBA
-            long unixTime = (pixel << 8) | ((pixel >> 24) & 0xFF);
-
-            assertThat(unixTime, is(1188518400L));
+            assertThat(readPixelValue(image, 0, 0), is(1188518400L));
         }
+    }
+
+    @Test
+    public void testLocal() throws Exception {
+        ReceivedResponse response = appUnderTest.getHttpClient().get("/api/1/local?now=2018-09-20T00:00:00Z&tz=Australia/Sydney");
+        assertThat(response.getHeaders().get("Content-Type"), is(equalTo("image/png")));
+        assertThat(response.getHeaders().get("Cache-Control"), is(equalTo("no-cache")));
+
+        try (InputStream body = response.getBody().getInputStream()) {
+            BufferedImage image = ImageIO.read(body);
+            assertThat(image.getWidth(), is(4));
+            assertThat(image.getHeight(), is(1));
+            assertThat(readPixelValue(image, 0, 0), is(1537401600L));
+            assertThat(readPixelValue(image, 1, 0), is(36000L));
+            assertThat(readPixelValue(image, 2, 0), is(1538841600L));
+            assertThat(readPixelValue(image, 3, 0), is(39600L));
+        }
+    }
+
+    private long readPixelValue(BufferedImage image, int x, int y) {
+        int pixel = image.getRGB(x, y);
+
+        // ARGB -> RGBA
+        return (pixel << 8) | ((pixel >> 24) & 0xFF);
     }
 }
